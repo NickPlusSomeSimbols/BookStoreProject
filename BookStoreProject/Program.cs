1defe986d4da5f597b29d7ff106a1a792b27b9e8
Mikael <mika-bagdasaryan@mail.ru>
2023-09-19 19:53:30 +0400

    SetUpSwagger

--- BookStoreProject/Program.cs
+++ BookStoreProject/Program.cs
@@ -5,20 +5,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
 var connectionString = builder.Configuration.GetConnectionString("Default");
 
 builder.Services.AddDbContext<BookStoreDbContext>(options =>
     options.UseSqlServer(connectionString));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
