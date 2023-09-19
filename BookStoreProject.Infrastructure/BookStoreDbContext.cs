96d2839f4ce03a2c7f71985143eb4a81aecbcf34
Mikael <mika-bagdasaryan@mail.ru>
2023-09-19 04:07:00 +0400

    DbContextFile_AlmostFinished

--- BookStoreProject.Infrastructure/BookStoreDbContext.cs
+++ BookStoreProject.Infrastructure/BookStoreDbContext.cs
@@ -16,5 +16,51 @@ namespace BookStoreProjectCore
         public DbSet<CatalogItem> CatalogItems { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.Property(i => i.Date).IsRequired();
+                entity.Property(i => i.Price).IsRequired();
+            });
+
+            modelBuilder.Entity<OrderItem>(entity =>
+            {
+                entity.HasOne(o => o.Order)
+                    .WithMany(i => i.Items)
+                    .HasForeignKey(o => o.OrderId);
+
+                entity.Property(i => i.Name).IsRequired();
+                entity.Property(i => i.Name).HasMaxLength(50);
+                entity.Property(i => i.Price).IsRequired();
+                entity.Property(i => i.Quantity).IsRequired();
+            });
+
+            modelBuilder.Entity<BasketItem>(entity =>
+            {
+                entity.HasOne(b => b.Basket)
+                    .WithMany(i => i.Items)
+                    .HasForeignKey(b => b.BasketId);
+
+                entity.Property(i => i.Name).IsRequired();
+                entity.Property(i => i.Name).HasMaxLength(50);
+                entity.Property(i => i.Price).IsRequired();
+                entity.Property(i => i.Quantity).IsRequired();
+                entity.HasIndex(i => i.CatalogItemId);
+            });
+
+            modelBuilder.Entity<CatalogItem>(entity =>
+            {
+                entity.Property(i => i.Name).IsRequired();
+                entity.Property(i => i.Name).HasMaxLength(50);
+                entity.Property(i => i.Description).IsRequired();
+                entity.Property(i => i.Description).HasMaxLength(200);
+                entity.Property(i => i.Price).IsRequired();
+                entity.Property(i => i.Quantity).IsRequired();
+            });
+        }
     }
 }
