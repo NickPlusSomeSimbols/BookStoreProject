6fe57c2e1675204e6a94cfd9fdb7603509466cbb
Mikael <mika-bagdasaryan@mail.ru>
2023-09-19 22:40:05 +0400

    ReplaceModels_NotComplete_1

--- BookStoreProject.Core/Model/Basket.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BookStoreProject.Model
-{
-    public class Basket : BaseEntity
-    {
-        public string? CustomerId { get; set; }
-        public ICollection<BasketItem>? Items { get; set; } = new List<BasketItem>();
-    }
-}
